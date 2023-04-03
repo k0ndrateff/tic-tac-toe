8aa5157263d49e61768a72733ac8789248f15f41
k0ndrateff <63606405+k0ndrateff@users.noreply.github.com>
2023-04-03 15:59:57 +0300

    Basic win checking functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,17 +2,74 @@ import React, {useState} from "react";
 import Cell from "./Cell";
 
 const Board:React.FC = () => {
+    let cellsInitialState = Array.from({length: 9}, () => {
+        return {
+            filling: '',
+            isWinning: false,
+        };
+    });
+
     let [turn, setTurn] = useState('x');
     let [filled, setFilled] = useState(0);
     let [visibilityRestart, setVisibilityRestart] = useState('none');
     let [visibilityText, setVisibilityText] = useState('inline');
-    let [cells, setCells] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    let [cells, setCells] = useState(cellsInitialState);
 
+
+    const changeCellFill = (cellIndex: number, filling: string) => {
+        cells[cellIndex].filling = filling;
+    };
+
+    const checkWinState = () => {
+        // Проверка горизонталей
+        for (let i = 0; i < 7; i+=3) {
+            if (cells[i].filling === cells[i + 1].filling &&
+                cells[i + 1].filling === cells[i + 2].filling &&
+                cells[i].filling !== ''
+            ) {
+                cells[i].isWinning = true;
+                cells[i + 1].isWinning = true;
+                cells[i + 2].isWinning = true;
+                return true;
+            }
+        }
+        // Проверка вертикалей
+        for (let i = 0; i < 3; i++) {
+            if (cells[i].filling === cells[i + 3].filling &&
+                cells[i + 3].filling === cells[i + 6].filling &&
+                cells[i].filling !== ''
+            ) {
+                cells[i].isWinning = true;
+                cells[i + 3].isWinning = true;
+                cells[i + 6].isWinning = true;
+                return true;
+            }
+        }
+        // Проверка диагоналей
+        if (cells[0].filling === cells[4].filling &&
+            cells[4].filling === cells[8].filling &&
+            cells[0].filling !== ''
+        ) {
+            cells[0].isWinning = true;
+            cells[4].isWinning = true;
+            cells[8].isWinning = true;
+            return true;
+        }
+        if (cells[2].filling === cells[4].filling &&
+            cells[4].filling === cells[6].filling &&
+            cells[2].filling !== ''
+        ) {
+            cells[2].isWinning = true;
+            cells[4].isWinning = true;
+            cells[6].isWinning = true;
+            return true;
+        }
+    }
 
     const updateCells = () => {
         setTurn(turn === 'x' ? '0' : 'x');
         setFilled(filled + 1);
-        if (filled === 8) {
+        if (checkWinState() || filled === 8) {
             setVisibilityRestart('block');
             setVisibilityText('none');
         }
@@ -23,7 +80,7 @@ const Board:React.FC = () => {
         setVisibilityText('inline');
         setFilled(0);
         setCells([]);
-        setTimeout(() => setCells([0, 1, 2, 3, 4, 5, 6, 7, 8]), 0);
+        setTimeout(() => setCells(cellsInitialState), 0);
     }
 
     return (
@@ -33,8 +90,13 @@ const Board:React.FC = () => {
             </span></h1>
             <button style={{display: visibilityRestart}} onClick={restartGame}>Переиграть</button>
             <div className={'board'}>
-                {cells.map(() => (
-                    <Cell currentTurn={turn} updateCells={updateCells} />
+                {cells.map((cell, index) => (
+                    <Cell currentTurn={turn}
+                          updateCells={updateCells}
+                          isWinningCell={cell.isWinning}
+                          cellIndex={index}
+                          changeFill={changeCellFill}
+                    />
                 ))}
             </div>
         </>
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -3,21 +3,33 @@ import React, {useState} from "react";
 type CellProps = {
     currentTurn: string;
     updateCells: any;
+    isWinningCell: boolean;
+    cellIndex: number;
+    changeFill: (index: number, val: string) => void;
 };
 
-const Cell:React.FC<CellProps> = ({ updateCells, currentTurn}) => {
+const Cell:React.FC<CellProps> = ({ updateCells, currentTurn, isWinningCell, cellIndex, changeFill}) => {
     let [element, setElement] = useState('');
     const handleClick = () => {
         if (element === '') {
             setElement(currentTurn);
             updateCells();
+            changeFill(cellIndex, currentTurn);
         }
     }
 
     return (
-        <div className={'cell'} onClick={handleClick}>
-            <img src={element ? element + '.png' : ''} alt={element} />
-        </div>
+        <>
+            {isWinningCell ? (
+                <div className={'cell winning'} onClick={handleClick}>
+                    <img src={element ? element + '.png' : ''} alt={element} />
+                </div>
+            ) : (
+                <div className={'cell'} onClick={handleClick}>
+                    <img src={element ? element + '.png' : ''} alt={element} />
+                </div>
+            )}
+        </>
     );
 };
 export default Cell;
